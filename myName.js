c9ec4f54821bd9b2f94554165e1202eb60bfc6e8
emkeator <emkeator@gmail.com>
2017-07-17 15:53:22 -0600

    targeting systems

--- myName.js
+++ myName.js
@@ -1,2 +1,16 @@
 var name = “Stardust”;
 var weaknesses = [“exhaust pipe”, “skywalker children”, “sand”];
+var targets = [
+  { "name": "Alderaan",
+    "reasons": "daughter didn't do her homework",
+    "coordinates": 23454
+  },
+  {"name": "Tatooine",
+    "reasons": "Darth Vader hates sand",
+    "coordinates": 9847
+  },
+  {"name": "Coruscant",
+    "reasons": "It's tacky and I hate it",
+    "coordinates": 349873
+  }
+];
